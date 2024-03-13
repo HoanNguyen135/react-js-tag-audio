10973b10d85a9c03154cf2c24a7e11daa192dca7
HoanNguyen <hoannguyen13520@gmail.com>
2024-03-13 13:46:48 +0700

    publish

---
+++ index.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const AudioTag = () => {
+  return (
+    <View>
+      <Text>AudioTag</Text>
+    </View>
+  );
+};
+
+export default AudioTag;
